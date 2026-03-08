0e24733938190cd81b4e6e89930a72eb88ba260a
t-hul <thomashultsch@web.de>
2026-03-08 20:13:24 +0100

    Add token usage output

--- main.py
+++ main.py
@@ -12,10 +12,19 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+    user_promt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    print(f"User prompt: {user_promt}")
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=user_promt,
     )
+
+    usage_metadata = response.usage_metadata
+    if usage_metadata is None:
+        raise RuntimeError("no usage_metadata")
+    print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {usage_metadata.candidates_token_count}")
 
     print("Response:")
     print(response.text)
