7e1304dc286c396b1c67211b5fcd917b3bf9fb11
t-hul <thomashultsch@web.de>
2026-03-08 20:01:37 +0100

    Initial commit, first request to Gemini API

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("GEMINI_API_KEY not found")
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
+
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
