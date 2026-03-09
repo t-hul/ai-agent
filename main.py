53d34a6067cac4ee7b05691e07fa72243958cc04
t-hul <thomashultsch@web.de>
2026-03-09 22:28:52 +0100

    Add option --verbose

--- main.py
+++ main.py
@@ -15,12 +15,11 @@ def main():
     client = genai.Client(api_key=api_key)
 
     parser = argparse.ArgumentParser(description="Gemini Chatbot")
-    parser.add_argument("user_promt", type=str, help="User prompt")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
-    user_promt = args.user_promt
-    print(f"User prompt: {user_promt}")
-    messages = [types.Content(role="user", parts=[types.Part(text=user_promt)])]
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(
         model="gemini-2.5-flash",
@@ -30,8 +29,11 @@ def main():
     usage_metadata = response.usage_metadata
     if usage_metadata is None:
         raise RuntimeError("no usage_metadata")
-    print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {usage_metadata.candidates_token_count}")
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {usage_metadata.candidates_token_count}")
 
     print("Response:")
     print(response.text)
